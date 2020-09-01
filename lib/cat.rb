0230758e52a7b838ba20299bb6debcb383068ff5
noaheakin <noaheakin@gmail.com>
2020-09-01 11:17:06 -0700

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Cat
-  # code goes here
+  
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
+
+# steve = Owner.new("Steve")
+# ix = Owner.new("Ix")
+# yuki = Dog.new("Yuki", steve)
+# gw = Dog.new("GW", steve)
+# rose = Dog.new("Rose", ix)
+
+# binding.pry
+# 0
